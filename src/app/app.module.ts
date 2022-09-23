63c8cfef2485983aacb74fe3a5212f55c3da1b8f
Vanessa Mercado <vanemercado305@gmail.com>
2022-09-23 13:31:07 -0400

    Tarea 1 Angular (Parte 1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{ ButtonModule } from 'primeng/button';
+import { AutoresComponent } from './autores/autores.component';
+import { LibrosComponent } from './libros/libros.component';
+import { MenubarModule } from 'primeng/menubar';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutoresComponent,
+    LibrosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    MenubarModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/libros/libros.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { LibrosService } from '../servicios/libros.service';
+
+@Component({
+  selector: 'app-libros',
+  templateUrl: './libros.component.html',
+  styleUrls: ['./libros.component.css']
+})
+export class LibrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
